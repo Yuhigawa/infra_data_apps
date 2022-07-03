ce4695a8ba7765c41e6b9796365e16edbd08c981
yuhigawa <pablostreiski@outlook.com>
2022-07-03 12:28:18 -0300

    fix: unnused buildschema

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,7 +5,7 @@ import { AppService } from './app.service';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { join } from 'path';
-import { DirectiveLocation, GraphQLDirective } from 'graphql';
+// import { DirectiveLocation, GraphQLDirective } from 'graphql';
 
 import { UsersModule } from './users/users.module';
 import { CommentsModule } from './comments/comments.module';
@@ -17,14 +17,6 @@ import { CommentsModule } from './comments/comments.module';
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
-      buildSchemaOptions: {
-        directives: [
-          new GraphQLDirective({
-            name: 'upper',
-            locations: [DirectiveLocation.FIELD_DEFINITION],
-          }),
-        ],
-      },
       sortSchema: true,
       debug: true,
       playground: true,
